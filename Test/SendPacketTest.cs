6bb1bec016110d1b51be8b44f596db3da66d2e69
Ayoub Kaanich <kayoub5@live.com>
2020-03-16 18:34:50 +0100

    Check Receive

--- Test/SendPacketTest.cs
+++ Test/SendPacketTest.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using static Test.TestHelper;
 using static System.TimeSpan;
+using System.Threading;
 
 namespace Test
 {
@@ -30,6 +31,35 @@ namespace Test
             CollectionAssert.AreEquivalent(packet.Bytes, received[0].Data);
         }
 
+        [Test]
+        public void TestPcapDeviceGetSequence()
+        {
+            CheckReceive(GetPcapDevice().Interface);
+
+            var loopInterface = LibPcapLiveDeviceList.Instance.FirstOrDefault(ni => ni.Loopback)?.Interface;
+            CheckReceive(loopInterface);
+
+            var anyInterface = LibPcapLiveDeviceList.Instance.FirstOrDefault(ni => ni.Name == "any")?.Interface;
+            CheckReceive(anyInterface);
+        }
+
+        private void CheckReceive(PcapInterface pcapInterface)
+        {
+            if (pcapInterface == null)
+            {
+                return;
+            }
+            var device = new LibPcapLiveDevice(pcapInterface);
+            Console.WriteLine("Using " + device);
+            device.Open(DeviceMode.Promiscuous, 0);
+            Thread.Sleep(5000);
+            foreach (var p in PcapDevice.GetSequence(device, false))
+            {
+                Console.WriteLine(p);
+            }
+            device.Close();
+        }
+
         [SetUp]
         public void SetUp()
         {
@@ -43,3 +73,4 @@ namespace Test
         }
     }
 }
+
